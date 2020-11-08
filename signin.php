8468841f65842d63f9f4d2cfea21d89089ef2b68
Ruth Trejo <rtrejo9@miners.utep.edu>
2020-11-07 22:59:38 -0700

    log-in authentication works now

--- signin.php
+++ signin.php
@@ -8,12 +8,14 @@ $conn = $databaseConnector->connect();
 $_SESSION['logged_in'] = false;
 $_SESSION['users_name'] = "";
 
+echo "connected to database<br>";
 if ( !empty($_POST) ){
+    echo "checking that POSt is not empty<br>";
     if( isset($_POST['submit']) ){
-
+        echo "checking the user submitted the button to submit<br>";
         //Store Clients credentials and prevent SQL Injections
         $input_username = stripslashes($_POST['uname']);
-        $input_password = mysqli_real_escape_string($_POST['pswd']);
+        $input_password = mysqli_real_escape_string($conn,$_POST['pswd']);
 
         //Select the username if found in database
         $result = mysqli_query($conn, "SELECT * FROM user WHERE username='$input_username'");
@@ -21,22 +23,40 @@ if ( !empty($_POST) ){
         $count = mysqli_num_rows($result);
         $row = $result -> fetch_array(MYSQLI_ASSOC);
 
+        echo "about to verify password<br>";
+        echo "hash password: " . $row['password'] . "<br>";
+        echo "users entered password: " . $input_password . "<br>";
+        echo "the username is: " . $row['username'] . "<br>";
         //VERIFY PASSWORD FROM USER
         if( $count == 1 ){
-            if( password_verify($input_password, $row['password']) ){
+            echo "count is 1 ---- now verifying password<br>";
+            if( password_verify('$input_password', $row['password']) ){
+                echo "password was verified <br>";
+                //Edit TIMESTAMP for latest login
+                $sql = "INSERT INTO user (last_login) VALUES (CURRENT_TIMESTAMP);";
+
+                if( mysqli_query($conn,$sql) ){
+                    echo "Successfully updates TIMESTAMP for latest login.";
+                }
+
+                /* Check whether the user is a regular user or an administrator */
                 if( $row['user_type'] == 'user' ){
                     $_SESSION['users_name'] = $input_username;
+                    $_SESSION['logged_in'] = true;
+                    $_SESSION['status'] = "User";
                     header("Location: user.php");
                 }
                 else if( $row['user_type'] == 'admin' ) {
                     $_SESSION['users_name'] = $input_username;
+                    $_SESSION['logged_in'] = true;
+                    $_SESSION['status'] = "Administrator";
                     header("Location: admin.php");
                 }
             }
-        }else{
-            echo "username or password is incorrect";
+        } else{
+            header("Location: mainpage.php");
         }
-
+        echo "never entered the password if statement";
 
     }
 }
@@ -64,7 +84,7 @@ if ( !empty($_POST) ){
             <label for="pswd"><b>Password</b></label>
             <input type="password" placeholder="Enter Password" name="pswd" required>
 
-            <button type="submit">Login</button>
+            <button id="submit" name="submit" type="submit">Login</button>
         </div>
 
         <div class="container" style="background-color:#f1f1f1">
